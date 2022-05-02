d2e0da29fd54d52df5a4e0e766009af10ed49344
ramshafirdous <104720378+ramshafirdous@users.noreply.github.com>
2022-05-02 19:20:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ def caesar(start_text, shift_amount, cipher_direction):
     else:
       end_text += char
   print(f"Here's the {cipher_direction}d result: {end_text}")
-    #TODO-3: What happens if the user enters a number/symbol/space?
-    #Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
+    #TODO-3: What happens if the user enters a number/symbol/space? fix the code to keep the number/symbol/space when the text is encoded/decoded?
     #e.g. start_text = "meet me at 3"
     #end_text = "•••• •• •• 3"
 
@@ -21,7 +20,7 @@ def caesar(start_text, shift_amount, cipher_direction):
 from art import logo
 print(logo)
 
-#TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
+#TODO-4: Ask the user if they want to restart the cipher program?
 #e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
 #If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
 #Hint: Try creating a while loop that continues to execute the program if the user types 'yes'. 
